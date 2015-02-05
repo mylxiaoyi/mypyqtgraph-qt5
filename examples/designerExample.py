1208a4c1a5c093e002b79dab13678475e512bad3
mylxiaoyi <mylxiaoyi@gmail.com>
2015-02-05 09:20:14 +0800

    examples/designerExample.py success

--- examples/designerExample.py
+++ examples/designerExample.py
@@ -10,7 +10,7 @@ tools/rebuildUi.py script).
 import initExample ## Add path to library (just for examples; you do not need this)
 
 import pyqtgraph as pg
-from pyqtgraph.Qt import QtCore, QtGui
+from pyqtgraph.Qt import QtCore, QtGui, QtWidgets
 import numpy as np
 import os
 
@@ -43,4 +43,4 @@ win = MainWindow()
 if __name__ == '__main__':
     import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
+        QtWidgets.QApplication.instance().exec_()
